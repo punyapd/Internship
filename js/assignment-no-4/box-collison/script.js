ce94769442ccc12f9b5fad483815c4b09e8d9c54
punyapd <dahalpp55@gmail.com>
2023-01-29 14:46:01 +0545

    box collison added(assmnt 4.1)

---
+++ js/assignment-no-4/box-collison/script.js
@@ -0,0 +1,158 @@
+const BOX_WIDTH = 40;
+const BOX_HEIGHT = 40;
+const CONTAINER_WIDTH = 600;
+CONTAINER_HEIGHT = 600;
+const SPEED = 10;
+const NUMBER_OF_BOXES = 10;
+
+function main() {
+  let body = document.querySelector("body");
+  let container = createContainer(body);
+  let boxes = [];
+  for (i = 0; i < NUMBER_OF_BOXES; i++) {
+    let box = createBox(container);
+    let overlaps = boxes.some((rect) => checkOverlap(box, rect));
+    while (overlaps) {
+      box = createBox(container);
+      overlaps = boxes.some((rect) => checkOverlap(box, rect));
+    }
+    drawBox(container, box.element);
+    boxes.push(box);
+  }
+  console.log("boxes: ", boxes);
+  setInterval(() => {
+    moveBoxes(boxes);
+    detectCollision(boxes);
+  }, 100);
+}
+
+//function for creating the main container
+function createContainer(parent) {
+  let container = document.createElement("div");
+  container.setAttribute("class", "container");
+  parent.appendChild(container);
+  return container;
+}
+
+function createBox(parent) {
+  let boxElement = document.createElement("div");
+  boxElement.style.width = BOX_WIDTH + "px";
+  boxElement.style.height = BOX_HEIGHT + "px";
+  boxElement.style.left =
+    Math.abs(Math.floor(Math.random() * CONTAINER_WIDTH - BOX_WIDTH)) + "px";
+  boxElement.style.top =
+    Math.abs(Math.floor(Math.random() * CONTAINER_HEIGHT - BOX_HEIGHT)) + "px";
+  boxElement.setAttribute("class", "box");
+  return {
+    element: boxElement,
+    directionX: 1,
+    directionY: 1,
+    INITIAL_POSTION_X: parseInt(boxElement.style.left),
+    INITIAL_POSTION_Y: parseInt(boxElement.style.top),
+  };
+}
+
+//function for drawing the boxes
+function drawBox(parent, boxElement) {
+  parent.appendChild(boxElement);
+}
+
+//function for checking the overlap of randomly generated boxes
+function checkOverlap(box1, box2) {
+  if (
+    box1.INITIAL_POSTION_X <= box2.INITIAL_POSTION_X &&
+    box1.INITIAL_POSTION_X + BOX_WIDTH >= box2.INITIAL_POSTION_X + BOX_WIDTH &&
+    box1.y <= box2.INITIAL_POSTION_Y &&
+    box1.INITIAL_POSTION_Y + BOX_HEIGHT >= box2.INITIAL_POSTION_Y + BOX_HEIGHT
+  ) {
+    return true;
+  }
+  // Check for partial overlap
+  if (
+    box1.INITIAL_POSTION_X < box2.INITIAL_POSTION_X + BOX_WIDTH &&
+    box1.INITIAL_POSTION_X + BOX_WIDTH > box2.INITIAL_POSTION_X &&
+    box1.INITIAL_POSTION_Y < box2.INITIAL_POSTION_Y + BOX_HEIGHT &&
+    box1.INITIAL_POSTION_Y + BOX_HEIGHT > box2.INITIAL_POSTION_Y
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function moveBoxes(boxes) {
+  boxes.forEach((box) => {
+    if (
+      parseInt(BOX_WIDTH) + parseInt(box.element.style.left) >=
+      CONTAINER_WIDTH
+    ) {
+      box.directionX = -1;
+    } else if (parseInt(box.element.style.left) <= 0) {
+      box.directionX = 1;
+    }
+
+    if (
+      parseInt(BOX_HEIGHT) + parseInt(box.element.style.top) >=
+      CONTAINER_HEIGHT
+    ) {
+      box.directionY = -1;
+    } else if (parseInt(box.element.style.top) <= 0) {
+      box.directionY = 1;
+    }
+
+    let NEW_POSITION_X = box.INITIAL_POSTION_X + box.directionX * SPEED;
+    let NEW_POSITION_Y = box.INITIAL_POSTION_Y + box.directionY * SPEED;
+    box.element.style.left = NEW_POSITION_X + "px";
+    box.element.style.top = NEW_POSITION_Y + "px";
+    box.INITIAL_POSTION_X = NEW_POSITION_X;
+    box.INITIAL_POSTION_Y = NEW_POSITION_Y;
+  });
+}
+
+//function for detecting the collison
+function detectCollision(boxes) {
+  for (let i = 0; i < boxes.length; i++) {
+    for (let j = i + 1; j < boxes.length; j++) {
+      let box1 = boxes[i];
+      let box2 = boxes[j];
+
+      // Check for horizontal collision
+      let collideHorizontal = checkHorzontalCollision(box1, box2);
+      if (collideHorizontal) {
+        box1.directionX = box1.directionX * -1;
+        box2.directionX = box2.directionX * -1;
+      }
+      // Check for vertical collision
+      let collideVertical = checkVerticalCollision(box1, box2);
+      if (collideVertical) {
+        box1.directionY = box1.directionY * -1;
+        box2.directionY = box2.directionY * -1;
+      }
+    }
+  }
+}
+
+//function for horizontal collison detection.
+function checkHorzontalCollision(box1, box2) {
+  if (
+    box1.INITIAL_POSTION_X < box2.INITIAL_POSTION_X + BOX_WIDTH &&
+    box1.INITIAL_POSTION_X + BOX_WIDTH > box2.INITIAL_POSTION_X &&
+    box1.INITIAL_POSTION_Y < box2.INITIAL_POSTION_Y + BOX_HEIGHT &&
+    BOX_HEIGHT + box1.INITIAL_POSTION_Y > box2.INITIAL_POSTION_Y
+  ) {
+    return true;
+  }
+}
+
+//function for vertical collilson detection.
+function checkVerticalCollision(box1, box2) {
+  if (
+    box1.INITIAL_POSTION_Y < box2.INITIAL_POSTION_Y + BOX_HEIGHT &&
+    box1.INITIAL_POSTION_Y + BOX_HEIGHT > box2.INITIAL_POSTION_Y &&
+    box1.INITIAL_POSTION_X < box2.INITIAL_POSTION_X + BOX_WIDTH &&
+    BOX_WIDTH + box1.INITIAL_POSTION_X > box2.INITIAL_POSTION_X
+  ) {
+    return true;
+  }
+}
+
+main();
--- pages/javascript.js
+++ pages/javascript.js
@@ -1,89 +1,107 @@
 assignment = [
-    {
-      number: 1,
-      title: "Assignment redo(DOM)",
-      codeLink:
-        "https://github.com/punyapd/Internship",
-      demoLink: "https://punyapd.github.io/Internship",
-    },
-    {
-      number: 2,
-  
-      title: "Password Generator",
-      codeLink:
-        "https://github.com/punyapd/Internship/tree/master/js/assignment-no-2/pg",
-      demoLink: "https://punyapd.github.io/Internship/js/assignment-no-2/pg/index.html",
-    },
-    {
-      number: 2,
-  
-      title: "clock and stopwatch",
-      codeLink:
-        "https://github.com/punyapd/Internship/tree/master/js/assignment-no-2/clock",
-      demoLink: "https://punyapd.github.io/Internship/js/assignment-no-2/clock/index.html",
-    },
-    {
-      number: 3,
-  
-      title: "Image carousel",
-      codeLink:
-        "https://github.com/punyapd/Internship/tree/master/js/assignment-no-3",
-      demoLink: "https://punyapd.github.io/Internship/js/assignment-no-3/index.html",
-    },
-   
-  ];
-  let main = document.getElementById("main");
-  
-  let container = document.createElement("div");
-  
-  container.setAttribute("class", "container");
-  main.appendChild(container);
-  
-  let h1 = document.createElement("h1");
-  let h1TextNode = document.createTextNode("JS Assignments");
-  h1.appendChild(h1TextNode);
-  container.appendChild(h1);
-  
-  let assmntCards = document.createElement("div");
-  assmntCards.setAttribute("class", "assignment__cards");
-  
-  //for loop starts here
-  assignment.map((item) => {
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
-  
-    let assmntCardsTitle = document.createElement("div");
-    assmntCardsTitle.setAttribute("class", "assignment__cards__title");
-  
-    let codeLinkTag = document.createElement("a");
-    codeLinkTag.setAttribute("href", item?.codeLink);
-    codeLinkTag.setAttribute("class", "link");
-    let codeLinkText = document.createTextNode("CODE");
-  
-    let demoLinkTag = document.createElement("a");
-    demoLinkTag.setAttribute("href", item?.demoLink);
-    demoLinkTag.setAttribute("class", "link");
-    let demoLinkText = document.createTextNode("DEMO");
-  
-    let numberHeader = document.createElement('h2');
-    let numberContent = document.createTextNode(item?.number)
-    numberHeader.appendChild(numberContent)
-  
-    let titleHeader = document.createElement("h3");
-    let titleContent = document.createTextNode(item?.title);
-    titleHeader.appendChild(titleContent);
-  
-    assmntCardsTitle.appendChild(numberHeader);
-  
-    assmntCardsTitle.appendChild(titleHeader);
-    card.appendChild(assmntCardsTitle);
-    assmntCards.appendChild(card);
-  
-    codeLinkTag.appendChild(codeLinkText);
-    card.appendChild(codeLinkTag);
-    demoLinkTag.appendChild(demoLinkText);
-    card.appendChild(demoLinkTag);
-  });
-  
-  container.appendChild(assmntCards); //append card list in container
-  
+  {
+    number: 1,
+    title: "Assignment redo(DOM)",
+    codeLink: "https://github.com/punyapd/Internship",
+    demoLink: "https://punyapd.github.io/Internship",
+  },
+  {
+    number: 2,
+
+    title: "Password Generator",
+    codeLink:
+      "https://github.com/punyapd/Internship/tree/master/js/assignment-no-2/pg",
+    demoLink:
+      "https://punyapd.github.io/Internship/js/assignment-no-2/pg/index.html",
+  },
+  {
+    number: 2,
+
+    title: "clock and stopwatch",
+    codeLink:
+      "https://github.com/punyapd/Internship/tree/master/js/assignment-no-2/clock",
+    demoLink:
+      "https://punyapd.github.io/Internship/js/assignment-no-2/clock/index.html",
+  },
+  {
+    number: 3,
+
+    title: "Image carousel",
+    codeLink:
+      "https://github.com/punyapd/Internship/tree/master/js/assignment-no-3",
+    demoLink:
+      "https://punyapd.github.io/Internship/js/assignment-no-3/index.html",
+  },
+  {
+    number: 4.1,
+
+    title: "Box Collison",
+    codeLink:
+      "https://github.com/punyapd/Internship/tree/master/js/assignment-no-4/box-collison",
+    demoLink:
+      "https://punyapd.github.io/Internship/js/assignment-no-4/box-collison/index.html",
+  },
+  {
+    number: 4.2,
+
+    title: "Ant Smasher",
+    codeLink:
+      "https://github.com/punyapd/Internship/tree/master/js/assignment-no-4/ant-smasher",
+    demoLink:
+      "https://punyapd.github.io/Internship/js/assignment-no-4/ant-smasher/index.html",
+  },
+];
+let main = document.getElementById("main");
+
+let container = document.createElement("div");
+
+container.setAttribute("class", "container");
+main.appendChild(container);
+
+let h1 = document.createElement("h1");
+let h1TextNode = document.createTextNode("JS Assignments");
+h1.appendChild(h1TextNode);
+container.appendChild(h1);
+
+let assmntCards = document.createElement("div");
+assmntCards.setAttribute("class", "assignment__cards");
+
+//for loop starts here
+assignment.map((item) => {
+  let card = document.createElement("div");
+  card.setAttribute("class", "card");
+
+  let assmntCardsTitle = document.createElement("div");
+  assmntCardsTitle.setAttribute("class", "assignment__cards__title");
+
+  let codeLinkTag = document.createElement("a");
+  codeLinkTag.setAttribute("href", item?.codeLink);
+  codeLinkTag.setAttribute("class", "link");
+  let codeLinkText = document.createTextNode("CODE");
+
+  let demoLinkTag = document.createElement("a");
+  demoLinkTag.setAttribute("href", item?.demoLink);
+  demoLinkTag.setAttribute("class", "link");
+  let demoLinkText = document.createTextNode("DEMO");
+
+  let numberHeader = document.createElement("h2");
+  let numberContent = document.createTextNode(item?.number);
+  numberHeader.appendChild(numberContent);
+
+  let titleHeader = document.createElement("h3");
+  let titleContent = document.createTextNode(item?.title);
+  titleHeader.appendChild(titleContent);
+
+  assmntCardsTitle.appendChild(numberHeader);
+
+  assmntCardsTitle.appendChild(titleHeader);
+  card.appendChild(assmntCardsTitle);
+  assmntCards.appendChild(card);
+
+  codeLinkTag.appendChild(codeLinkText);
+  card.appendChild(codeLinkTag);
+  demoLinkTag.appendChild(demoLinkText);
+  card.appendChild(demoLinkTag);
+});
+
+container.appendChild(assmntCards); //append card list in container

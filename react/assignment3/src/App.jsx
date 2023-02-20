4431172ae4befe17cd59abf4adca85c31045c04a
punyapd <dahalpp55@gmail.com>
2023-02-20 16:49:52 +0545

    ...

--- react/assignment3/src/App.jsx
+++ react/assignment3/src/App.jsx
@@ -1,22 +1,29 @@
-import './app.scss';
-import './icon/style.scss';
-import './icon/variables.scss';
-import './assignments/components/dashboard/_dashboard.scss'
-import './assignments/components/sidebar/sidebar.scss';
-import './assignments/components/Tickets/_tickets.scss';
-import './assignments/components/Tickets/_ticketTable.scss'
-import './assignments/components/modals/_modal.scss'
-import './form.scss';
-import './assignments/components/reusable/_button.scss'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./assignments/pages/Login";
+
+import "./app.scss";
+import "./icon/style.scss";
+import "./icon/variables.scss";
+import "./assignments/components/dashboard/_dashboard.scss";
+import "./assignments/components/sidebar/sidebar.scss";
+import "./assignments/components/Tickets/_tickets.scss";
+import "./assignments/components/Tickets/_ticketTable.scss";
+import "./assignments/components/modals/_modal.scss";
+import "./form.scss";
+import "./assignments/components/reusable/_button.scss";
 import Dashboard from "./assignments/components/dashboard/Dashboard";
-import Login from './assignments/pages/Login';
 function App() {
-
   return (
-   <Dashboard/>
-  // <Login />
-
-  )
+    <>
+      {" "}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
--- react/assignment3/src/main.jsx
+++ react/assignment3/src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import Login from "./assignments/pages/Login";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      <App />
+  </React.StrictMode>
+);
